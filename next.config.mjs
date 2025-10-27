1d0b7585f2e2580db92901205134efb36d71b051
jiyun0522 <wnlemforhs24@gmail.com>
2025-10-27 11:17:01 +0900

    ✨ Feat: products/[id].js → items/[id].js로 라우트 변경 및 리다이렉트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/products/:id",
+        destination: "/items/:id",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

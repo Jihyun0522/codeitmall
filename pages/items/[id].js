200990cfc554785606de68827ffb25c275ca87bc
jiyun0522 <wnlemforhs24@gmail.com>
2025-10-28 14:19:53 +0900

    ♻️ Refactor: <img> 태그를 <Image> 컴포넌트로 변경

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -6,6 +6,7 @@ import SizeReviewList from "@/components/SizeReviewList";
 import StarRating from "@/components/StarRating";
 import Header from "@/components/Header";
 import Container from "@/components/Container";
+import Image from "next/image";
 
 export default function Product() {
   const [product, setProduct] = useState();
@@ -41,12 +42,8 @@ export default function Product() {
         <span className={styles.englishName}>{product.englishName}</span>
       </h1>
       <div className={styles.content}>
-        <div>
-          <img
-            className={styles.image}
-            src={product.imgUrl}
-            alt={product.name}
-          />
+        <div className={styles.image}>
+          <Image fill src={product.imgUrl} alt={product.name} />
         </div>
         <div>
           <section className={styles.section}>
